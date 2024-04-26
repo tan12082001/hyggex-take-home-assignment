7e41138718911c3609759f80dc3979e552f34e6b
Tanmayi Manku <tanmayitanum@gmail.com>
2024-04-26 20:37:06 +0530

    Remove the comments

--- src/components/PathDisplayComponent.js
+++ src/components/PathDisplayComponent.js
@@ -1,4 +1,3 @@
-// import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import HomeIcon from '../assets/homeicon.png';
@@ -7,19 +6,7 @@ const PathDisplayComponent = ({ subject, topic }) => {
   const location = useLocation();
   const currentPath = location.pathname;
   const pathName = currentPath.replace('/', ' ˃ ');
-  // const data = useSelector((state) => state.subjects.subjects);
 
-  // console.log(`data is: ${data}`);
-  // console.log(`data is: ${JSON.stringify(data)}`);
-  // console.log(data[0].topics[0].flashcards);
-  // data.forEach((item, index) => {
-  //   console.log(`Item ${index}:`, item.subjectName);
-  //   console.log(`item ${index}: `, item.topics);
-  //   item.topics.forEach((item, index) => {
-  //     console.log(`topics: ${index}`, item.topicName);
-  //   });
-  // });
-  // console.log(`current path is: ${currentPath} and name is ${pathName}`);
   return (
     <div className="path-display-outer">
       <img src={HomeIcon} alt="Home" className="home-icon" />

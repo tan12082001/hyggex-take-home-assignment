53b91305c2343a3df1c149b03396f4f987c1041b
Tanmayi Manku <tanmayitanum@gmail.com>
2024-04-26 12:16:25 +0530

    Implement slideshow effect to flashcards

--- src/components/FlashCardStudy.js
+++ src/components/FlashCardStudy.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import HintIcon from '../assets/hinticon.png';
 import AudioIcon from '../assets/audioicon.png';
@@ -35,6 +36,29 @@ const FlashCardStudy = () => {
     },
   ];
 
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === study.length - 1 ? 0 : currentSlide + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? study.length - 1 : currentSlide - 1);
+  };
+
+  const slideStyles = {
+    display: 'flex',
+    width: '100%',
+    overflowX: 'hidden',
+  };
+
+  const eachSlideStyles = {
+    flex: '0 0 auto',
+    width: '100%',
+    transition: 'transform 0.5s ease',
+    transform: `translateX(-${currentSlide * 100}%)`,
+  };
+
   return (
     <div className="flashcards-screen">
       <div className="flashcards-slides">
@@ -42,16 +66,25 @@ const FlashCardStudy = () => {
           <img src={HintIcon} alt="Hint" className="flashcard-hint" />
           <img src={AudioIcon} alt="Audio" className="flashcard-audio" />
         </div>
-        <div className="flashcard-content">
-          {study.map(({
-            content,
-          }) => (
-            <div key={uuidv4()} className="each-slide">{content}</div>
+        <div className="flashcard-content" style={slideStyles}>
+          {study.map(({ content }) => (
+            <div key={uuidv4()} className="each-slide" style={eachSlideStyles}>
+              {content}
+            </div>
           ))}
         </div>
       </div>
       <div className="flashcard-footer">
         <img src={AgainIcon} alt="again" className="flashcard-again" />
+        <div className="previous-next-count-outer">
+          <button type="button" className="next-slide-button" onClick={nextSlide}>&#x2c3;</button>
+          <div className="slide-count">
+            {currentSlide + 1}
+            /
+            {study.length}
+          </div>
+          <button type="button" className="previous-slide-button" onClick={prevSlide}>&#x2c2;</button>
+        </div>
         <img src={MaxIcon} alt="maximize" className="flashcard-maximize" />
       </div>
     </div>
